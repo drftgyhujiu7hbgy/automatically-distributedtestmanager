6df1f884b14dcc6c161f4f7f33bec03962bc5298
drftgyhujiu7hbgy <js1rozenewe@hotmail.com>
2024-05-07 00:31:37 +0700

    update project

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-function longestPalindromeSubseq(s) {
-  const n = s.length;
-  const dp = Array.from(Array(n), () => Array(n).fill(0));
-  for (let i = 0; i < n; i++) {
-    dp[i][i] = 1;
+function minWindowSubstring(s, t) {
+  const map = new Map();
+  for (const char of t) {
+    map.set(char, (map.get(char) || 0) + 1);
   }
-  for (let len = 2; len <= n; len++) {
-    for (let i = 0; i < n - len + 1; i++) {
-      const j = i + len - 1;
-      if (s[i] === s[j]) {
-        dp[i][j] = 2 + dp[i + 1][j - 1];
-      } else {
-        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-      }
+  let required = map.size;
+  let left = 0;
+  let right = 0;
+  let minLen = Infinity;
+  let substrStart = 0;
+  while (right < s.length) {
+    const char = s[right];
+    if (map.has(char)) {
+      map.set(char, map.get(char) - 1);
+      if (map.get(char) === 0) required--;
     }
+    while (required === 0) {
+      if (right - left + 1 < minLen) {
+        minLen = right - left + 1;
+        substrStart = left;
+      }
+      const leftChar = s[left];
+      if (map.has(leftChar)) {
+        map.set(leftChar, map.get(leftChar) + 1);
+        if (map.get(leftChar) > 0) required++;
+      }
+      left++;
+    }
+    right++;
   }
-  return dp[0][n - 1];
+  return minLen === Infinity
+    ? ""
+    : s.substring(substrStart, substrStart + minLen);
 }
