3fc12f13f7c0f02bb4a4b02f4e7a2e71c6e257aa
drftgyhujiu7hbgy <js1rozenewe@hotmail.com>
2024-05-14 07:57:43 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const fibonacci = (n) => {
-  const sequence = [0, 1];
-  for (let i = 2; i <= n; i++) {
-    sequence.push(sequence[i - 1] + sequence[i - 2]);
+function combinationSum2(candidates, target) {
+  candidates.sort((a, b) => a - b);
+  const result = [];
+  backtrack([], 0, 0);
+  return result;
+  function backtrack(combination, start, sum) {
+    if (sum === target) {
+      result.push([...combination]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      if (i > start && candidates[i] === candidates[i - 1]) continue;
+      combination.push(candidates[i]);
+      backtrack(combination, i + 1, sum + candidates[i]);
+      combination.pop();
+    }
   }
-  return sequence;
-};
+}
